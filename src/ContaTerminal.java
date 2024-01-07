55dc458259a84d0463253c5b9a2acc6925482255
edu-vini <vinyeduardo95@gmail.com>
2024-01-07 15:44:01 -0300

    ContaTerminal Completa

--- src/ContaTerminal.java
+++ src/ContaTerminal.java
@@ -1,6 +1,29 @@
+import java.util.Locale;
+import java.util.Scanner;
+
 public class ContaTerminal {
+
+    static int numero;
+    static String agencia;
+    static String nomeCliente;
+    static double saldo;
+
     public static void main(String[] args) {
 
-        System.out.println("Hello world!");
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Por favor, digite o número da Conta!");
+        numero = scanner.nextInt();
+
+        System.out.println("Por favor digite o número da Agência!");
+        agencia = scanner.next();
+
+        System.out.println("Por favor digite o nome do Cliente!");
+        nomeCliente = scanner.next();
+
+        System.out.println("Por favor digite o Saldo da conta!");
+        saldo = scanner.nextDouble();
+
+        System.out.printf(Locale.GERMAN, "Olá %s, obrigado por criar uma conta em nosso banco, sua agência é %s, conta %d e seu saldo %.2f já está disponível para saque",nomeCliente, agencia, numero, saldo );
     }
 }
